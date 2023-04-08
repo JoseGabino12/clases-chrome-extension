d062362f717ff36ffe1534ed0a3efd5cf0c25c36
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-04-07 22:45:56 -0600

    Reto completado

--- popup/index.js
+++ popup/index.js
@@ -1,13 +1,16 @@
-const btnScripting = document.getElementById("btnscript");
+const btnScripting = document.getElementById('btnscript')
+const divLoading = document.getElementById('divLoading')
 
-btnScripting.addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+btnScripting.addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: alertHelloWorld,
-  });
-});
+    func: alertHelloWorld
+  })
+})
 
 function alertHelloWorld() {
-  alert("Hello World");
+  const cards = document.querySelectorAll('[id^=jobcard-]')
+  divLoading.style.display = 'flex'
+  console.log(cards)
 }
